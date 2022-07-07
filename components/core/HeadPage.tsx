61b5db352b9a889eb74ad394aabc19569734be62
YoLoN <shadyolown13@onet.pl>
2022-07-07 21:01:38 +0200

    :sparkles: Add HeadPage component

---
+++ components/core/HeadPage.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+
+const HeadPage = () => {
+    return (
+        <Head>
+            <meta charSet="UTF-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
+
+            <meta name="author" content="Dawid Oleksiuk"/>
+            <meta name="description" content="Discover my Portfolio to get information about me and also inspiration."/>
+            <meta property="og:title" content="AkaLientesS" />
+            <meta property="og:type" content="website" />
+            <meta property="og:description" content="Discover my Portfolio to get information about me and also inspiration." />
+            <meta property="og:locale" content="pl_PL" />
+            <meta property="og:image" content="/seoAkalientess.png" />
+            <meta property="og:url" content="https://www.akalientess.com/"/>
+            <meta property="og:locale:alternate" content="en_US" />
+            <meta name="twitter:site" content="@DawidOleksiuk"/>
+            <meta name="twitter:title" content="AkaLientesS"/>
+            <meta name="twitter:description" content="Discover my Portfolio to get information about me and also inspiration"/>
+            <meta name="twitter:image" content="/seoAkalientess.png" />
+            <meta name="format-detection" content="telephone=no"/>
+            <link rel="icon" type="image/png" href="/favicon.png" />
+            <link rel="apple-touch-icon" href="/favicon.png" />
+
+            <title>AkaLientesS</title>
+        </Head>
+    )
+}
+
+export default HeadPage
