4f9dc6978223257dcfa7b2e42e1bdd0d7e7e2982
YoLoN <shadyolown13@onet.pl>
2022-07-24 13:02:58 +0200

    :sparkles: Add notifications to contact form

--- components/contact/ContactForm.tsx
+++ components/contact/ContactForm.tsx
@@ -7,9 +7,11 @@ import MessageAreaField from "../../elements/inputFields/MessageAreaField";
 import {ContactDataInterface} from "../../interfaces/ContactDataInterface";
 import {useInView} from "react-intersection-observer";
 import {contact} from "../../schemas/validation/contact";
+import {useState} from "react";
 
 const ContactForm = () => {
     const { ref, inView } = useInView();
+    const [notification, setNotification] = useState<string>('');
 
     const submitHandler = (data: ContactDataInterface, {setSubmitting, resetForm}: {setSubmitting: Function, resetForm: Function}) => {
         axios({
@@ -17,37 +19,54 @@ const ContactForm = () => {
             url: process.env.NEXT_PUBLIC_CONTACT_URL,
             data: data
         }).then(_ => {
+            setNotification('successfully')
+
+            setTimeout(() => {
+                setNotification('')
+            }, 5000)
+
             setSubmitting(false)
             resetForm(true)
         }).catch(_ => {
+            setNotification('failed')
+
+            setTimeout(() => {
+                setNotification('')
+            }, 5000)
+
             setSubmitting(false)
         })
     }
 
     return (
-        <Formik initialValues={{name: '', email: '', subject: '', message: ''}} onSubmit={submitHandler} validationSchema={contact} >
-            {({ isSubmitting }) => (
-                <Form ref={ref} className={`${inView && 'is-active'}`}>
-                    <div className="row row--double">
-                        <div>
-                            <NameField name="name" />
+        <>
+            <div className={`notification notification--${notification}${notification && ' is-active'}`}>
+                {notification === 'successfully' ? 'Thank you for contacting me, I will answer you soon' : 'Something went wrong. Please try again later'}
+            </div>
+            <Formik initialValues={{name: '', email: '', subject: '', message: ''}} onSubmit={submitHandler} validationSchema={contact} >
+                {({ isSubmitting }) => (
+                    <Form ref={ref} className={`${inView && 'is-active'}`}>
+                        <div className="row row--double">
+                            <div>
+                                <NameField name="name" />
+                            </div>
+                            <div>
+                                <EmailField name="email" />
+                            </div>
                         </div>
-                        <div>
-                            <EmailField name="email" />
+                        <div className="row row--full">
+                            <SubjectField name="subject" />
                         </div>
-                    </div>
-                    <div className="row row--full">
-                        <SubjectField name="subject" />
-                    </div>
-                    <div className="row row--full">
-                        <MessageAreaField name="message" />
-                    </div>
-                    <button className="button" disabled={isSubmitting} type="submit">
-                        <span>Submit</span>
-                    </button>
-                </Form>
-            )}
-        </Formik>
+                        <div className="row row--full">
+                            <MessageAreaField name="message" />
+                        </div>
+                        <button className="button" disabled={isSubmitting} type="submit">
+                            <span>Submit</span>
+                        </button>
+                    </Form>
+                )}
+            </Formik>
+        </>
     )
 }
 
