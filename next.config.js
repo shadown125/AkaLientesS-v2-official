4a6b0d1215fb563254e3e2eab10fc7aeff9073a7
YoLoN <shadyolown13@onet.pl>
2022-07-25 21:49:27 +0200

    :art: Improve and fix not set CSP headers

--- next.config.js
+++ next.config.js
@@ -1,42 +1,64 @@
-const { createSecureHeaders } = require("next-secure-headers");
 const { SubresourceIntegrityPlugin } = require('webpack-subresource-integrity');
+const { i18n } = require('./next-i18next.config');
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval';
+  style-src 'self' 'unsafe-inline';
+  font-src 'self';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'none';
+  connect-src 'self' https://formspree.io/;
+`
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en"
-  },
   async headers() {
     return [
       {
-        source: "/(.*)",
-        headers: createSecureHeaders({
-          contentSecurityPolicy: {
-            directives: {
-              defaultSrc: [
-                "'self'",
-              ],
-              styleSrc: ["'self'", "'unsafe-inline'"],
-              imgSrc: ["'self'"],
-              baseUri: "self",
-              formAction: "self",
-              frameAncestors: true
-            }
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
           },
-          frameGuard: "deny",
-          noopen: "noopen",
-          nosniff: "nosniff",
-          xssProtection: "sanitize",
-          forceHTTPSRedirect: [
-            true,
-            { maxAge: 60 * 60 * 24 * 360, includeSubDomains: true },
-          ],
-          referrerPolicy: "same-origin"
-        })
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'X-Download-Options',
+            value: 'noopen'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'same-origin'
+          }
+        ],
       }
-    ];
+    ]
   },
   webpack(config) {
     config.output.crossOriginLoading = "anonymous";
