8e16d4ae77d218ef3315242fa46a860e250541f5
YoLoN <shadyolown13@onet.pl>
2022-08-07 12:42:02 +0200

    :fire: Remove client rendering on app
    It is not necessary anymore

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,31 +3,17 @@ import type { AppProps } from "next/app";
 import HeadPage from "../components/core/HeadPage";
 import Footer from "../components/core/Footer";
 import { InitialLoadContextProvider } from "../components/context/initialLoadContext";
-import { useEffect, useState } from "react";
 import { appWithTranslation } from "next-i18next";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    const [load, setLoad] = useState<boolean>(false);
-
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-            setLoad(true);
-            return;
-        }
-    }, [load, setLoad]);
-
     return (
-        <>
-            {load && (
-                <InitialLoadContextProvider>
-                    <div className="app">
-                        <HeadPage />
-                        <Component {...pageProps} />
-                        <Footer />
-                    </div>
-                </InitialLoadContextProvider>
-            )}
-        </>
+        <InitialLoadContextProvider>
+            <div className="app">
+                <HeadPage />
+                <Component {...pageProps} />
+                <Footer />
+            </div>
+        </InitialLoadContextProvider>
     );
 }
 
