ff28d34a284cc7d7f4f49c4c21c78ac3a95ee438
YoLoN <shadyolown13@onet.pl>
2022-08-08 19:18:55 +0200

    :hammer: Change seo image naming

--- components/core/HeadPage.tsx
+++ components/core/HeadPage.tsx
@@ -13,13 +13,13 @@ const HeadPage = () => {
             <meta property="og:type" content="website" />
             <meta property="og:description" content="Discover my Portfolio to get information about me and also inspiration." />
             <meta property="og:locale" content="pl_PL" />
-            <meta property="og:image" content="/seoAkalientess.jpg" />
+            <meta property="og:image" content="/akaLientessSeo.jpg" />
             <meta property="og:url" content="https://www.akalientess.com/" />
             <meta property="og:locale:alternate" content="en_US" />
             <meta name="twitter:site" content="@DawidOleksiuk" />
             <meta name="twitter:title" content="AkaLientesS" />
             <meta name="twitter:description" content="Discover my Portfolio to get information about me and also inspiration" />
-            <meta name="twitter:image" content="/seoAkalientess.jpg" />
+            <meta name="twitter:image" content="/akaLientessSeo.jpg" />
 
             <link rel="alternate" href="https://www.akalientess.com/en" hrefLang="en" />
             <link rel="alternate" href="https://www.akalientess.com/pl" hrefLang="pl" />
