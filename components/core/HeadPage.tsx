0b743987f25a91fdae6d20c3de565a4e14a22db7
YoLoN <shadyolown13@onet.pl>
2022-08-08 19:32:15 +0200

    :alien: Change and translate meta description tags

--- components/core/HeadPage.tsx
+++ components/core/HeadPage.tsx
@@ -1,6 +1,9 @@
 import Head from "next/head";
+import { useTranslation } from "next-i18next";
 
 const HeadPage = () => {
+    const { t } = useTranslation("common");
+
     return (
         <Head>
             <meta charSet="UTF-8" />
@@ -8,17 +11,17 @@ const HeadPage = () => {
             <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
 
             <meta name="author" content="Dawid Oleksiuk" />
-            <meta name="description" content="Discover my Portfolio to get information about me and also inspiration." />
+            <meta name="description" content={t("metaDescription")} />
             <meta property="og:title" content="AkaLientesS" />
             <meta property="og:type" content="website" />
-            <meta property="og:description" content="Discover my Portfolio to get information about me and also inspiration." />
+            <meta property="og:description" content={t("metaDescription")} />
             <meta property="og:locale" content="pl_PL" />
             <meta property="og:image" content="/akaLientessSeo.jpg" />
             <meta property="og:url" content="https://www.akalientess.com/" />
             <meta property="og:locale:alternate" content="en_US" />
             <meta name="twitter:site" content="@DawidOleksiuk" />
             <meta name="twitter:title" content="AkaLientesS" />
-            <meta name="twitter:description" content="Discover my Portfolio to get information about me and also inspiration" />
+            <meta name="twitter:description" content={t("metaDescription")} />
             <meta name="twitter:image" content="/akaLientessSeo.jpg" />
 
             <link rel="alternate" href="https://www.akalientess.com/en" hrefLang="en" />
